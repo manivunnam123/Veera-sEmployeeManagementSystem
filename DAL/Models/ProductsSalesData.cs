b9334b5dbe4c4152e5722c62daf7b441b7372d29
mani <vunnamveeramanendra@gmail.com>
2026-03-02 11:10:23 -0500

    Revert "hiii"
    This reverts commit 30a65a144cd55b3d885ff07cc1fd32f962e33ef3.

--- DAL/Models/ProductsSalesData.cs
+++ DAL/Models/ProductsSalesData.cs
@@ -3,7 +3,7 @@
     public class ProductsSalesData
     {
         public int ProductID { get; set; }
-      
+        public string ProductNameS { get; set; }
         public int Sales { get; set; }
 
     }
